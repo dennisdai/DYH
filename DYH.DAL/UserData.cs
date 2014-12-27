e67863e7677df697dd94173879c501e57dfb62d7
dennisdai <280141563@qq.com>
2014-12-28 01:06:19 +0800

    update

---
+++ DYH.DAL/UserData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DYH.Core;
+using DYH.Data;
+using DYH.IDAL;
+using DYH.Models;
+
+namespace DYH.DAL
+{
+    public class UserData : IUser
+    {
+        private readonly DbConn _conn;
+        public UserData(DbConn conn)
+        {
+            _conn = conn;
+        }
+
+        public UserEntry GetUser(int userId)
+        {
+            return _conn.Database.FirstOrDefault<UserEntry>("WHERE userid = @0", userId);
+        }
+
+        public UserEntry GetUser(string userName)
+        {
+            return _conn.Database.FirstOrDefault<UserEntry>("WHERE username = @0", userName);
+        }
+
+        public int Add(UserEntry entry)
+        {
+            return DataCast.Get<int>(_conn.Database.Insert(entry));
+        }
+
+        public int Update(UserEntry entry)
+        {
+            return _conn.Database.Update(entry);
+        }
+
+        public int Delete(UserEntry entry)
+        {
+            return _conn.Database.Delete(entry);
+        }
+
+        public IEnumerable<UserEntry> GetList()
+        {
+            return _conn.Database.Query<UserEntry>("WHERE 1=1");
+        }
+    }
+}
---
+++ DYH.IDAL/IUsers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DYH.Models;
+
+namespace DYH.IDAL
+{
+    public interface IUser
+    {
+        IEnumerable<UserEntry> GetList();
+        UserEntry GetUser(int userId);
+        UserEntry GetUser(string userName);
+        int Add(UserEntry entry);
+        int Update(UserEntry entry);
+        int Delete(UserEntry entry);
+    }
+}
